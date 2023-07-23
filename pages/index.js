74497d1d7536b43a364ebf0ff00aef30cf436eae
raejin <raejin0@naver.com>
2023-07-23 20:05:46 +0900

    add index file

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home( { allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hi I'm one of a kind</p>
+        <p>Hi I'm one of a kind - test test</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)          
